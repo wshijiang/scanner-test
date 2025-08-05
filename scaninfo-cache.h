0d713d84fdc07397d4596b18b35007859f15a271
wangshijiang <wshijiang1@outlook.com>
2025-08-05 17:04:11 +0800

    修改错误处

--- scaninfo-cache.h
+++ scaninfo-cache.h
@@ -14,7 +14,7 @@ CacheManager* create_cache_manager(void);
 IPEntry* create_ip_entry(const char*, unsigned, const char*);
 int expend_info_arry(IPEntry*);
 int add_data_to_entry(IPEntry*, \
-    unsigned short, const char*, const char*, const char*, const char*);
+    unsigned short, const char*, const char*, const char*);
 int add_scan_result_to_cache(CacheManager*, const char*, const char*, \
     unsigned short, const char*, const char*, const char*);
 int write_to_database(const PGconn*, const CacheManager*);
